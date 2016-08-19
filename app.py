3643e4c11bee7a284285203fa34f3c2e92be9ed9
Nikhil Sardana <nikhilklk@gmail.com>
2016-08-19 17:35:02 +0530

    finished app

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask
 from flask import request,redirect,render_template,url_for
+from werkzeug.utils import secure_filename
+import os
+import pytesseract
+from PIL import ImageFilter
+from PIL import Image
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './media'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/submitImage/',methods=['POST',])
+def submitImage():
+    image = request.files['ocrImage']
+    text = ''
+    filename = secure_filename(image.filename)
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    text = pytesseract.image_to_string(img)
+    return render_template('textFile.html',text=text)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',8000)
-    app.run(debug=True)
